14ce44f334e521556a1a091d957f7c68c95b724f
Nayeem Uddin <unayeem33@gmail.com>
2025-02-21 21:29:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config(); // Load environment variables from .env
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.hl8mn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the MongoDB server
+    // await client.connect();
+
+    // Access the desired database
+    const database = client.db("Task-it-DB");
+    const tasks = database.collection("Tasks");
+    const users = database.collection("Users");
+    const activity = database.collection("Activity");
+
+    //** Adding a new Task
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await tasks.insertOne(task);
+      res.send(result);
+    });
+
+    // Example API route: Check server connection
+    app.get("/", (req, res) => {
+      res.send("Hello Worlds!");
+    });
+
+    // Add more API routes for database interaction here
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+// Start the server and connect to the database
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
